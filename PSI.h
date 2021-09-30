860c434b3fca8c8cc2d3feb3654e39963b7204e8
Stefan Lueders <github@vetinari.eu>
2021-09-30 09:42:23 +0200

    Minor fixes

--- PSI.h
+++ PSI.h
@@ -18,20 +18,44 @@ struct distpoint {
     }
 };
 
-void psi_init();
-void psi_set_clamp_values(double * cmins, double * cmaxs);
-int psi_read_points(std::string cool_file, int apply_log_to_vals);
-int psi_construct_btree(double ** points);
-PSIBallTree * psi_construct_btree_recursive(double ** base, int * indices, int n);
-int * psi_find_k_nearest_neighbor(double * target, int k);
-int * psi_find_k_nearest_neighbor(double * target, double ** points, PSIBallTree * btree, int k);
-int * psi_find_k_nearest_neighbour_bruteforce(double * target, int k);
-double get_dist(double * target, int index);
-double dot(double * v, double * w);
-int * psi_projective_simplex_algorithm(int * neighbors, double * target, int k);
-double get_coord(int i, int j);
-double get_val(int i, int j);
-int * psi_adaptive_projective_simplex_algorithm(double * target, int k, double factor, int max_steps);
-int get_nlim();
-double * psi_interpolate(double * target);
+
+class PSI {
+private:
+    double ** coords;
+    double ** vals;
+    double CLAMP_MIN[DIP_DIMS];
+    double CLAMP_MAX[DIP_DIMS];
+    int N_LIM;
+    PSIBallTree * btree;
+    
+    double dot(double * v, double * w);
+    PSIBallTree * construct_btree_recursive(double ** base, int * indices, int n);
+    void find_k_nearest_neighbour_recursive(PSIBallTree * root, const double * target, double ** base,
+                                            std::priority_queue <distpoint> * Q, int k);
+    
+public:
+    PSI() {
+      coords = new double * [DIP_NMAX];
+      vals = new double * [DIP_NMAX];
+  
+      for (int i = 0; i < DIP_NMAX; i++) {
+        coords[i] = new double[DIP_DIMS];
+        vals[i]   = new double[DIP_VARNR];
+      }
+    };
+    
+    int read_files(std::string cool_file);
+    int get_nlim();
+    double get_coord(int i, int j);
+    double get_val(int i, int j);
+    double get_dist(double * target, int index);
+    void set_clamp_values(double * cmins, double * cmaxs);
+    int construct_btree(double ** points);
+    int * find_k_nearest_neighbour(double * target, double ** points, PSIBallTree * btree, int k);
+    int * find_k_nearest_neighbour(double * target, int k);
+    int * projective_simplex_algorithm(int * neighbours, double * target, int k);
+    int * adaptive_projective_simplex_algorithm(double * target, int k, double factor, int max_steps);
+    double * interpolat(double * target);
+};
+
 #endif //DIP_PSI_H
